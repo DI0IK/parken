7025f80f0ce8a0c1b8318ae5da5b787c13055517
Dominik Stahl <dominik@samdj.de>
2025-01-18 00:00:53 +0100

    Remove unnecessary timezone offset calculation in data fetching logic for improved accuracy

--- client/script.js
+++ client/script.js
@@ -5,8 +5,6 @@ document.getElementById("date-select").value = new Date()
 let chartInstance = null;
 let updateInterval = null;
 
-const timeZoneOffset = new Date().getTimezoneOffset() * 60 * 1000;
-
 async function fetchGarages() {
   const response = await fetch("/api/garages");
   const garages = await response.json();
@@ -34,8 +32,7 @@ async function fetchGarages() {
 }
 
 async function fetchData(id, date, offset = 0) {
-  const end =
-    new Date(date).getTime() + timeZoneOffset - offset + 24 * 60 * 60 * 1000;
+  const end = new Date(date).getTime() - offset + 24 * 60 * 60 * 1000;
   const start = new Date(end - 24 * 60 * 60 * 1000).getTime();
   const response = await fetch(`/api/data/${id}?start=${start}&end=${end}`);
   const { data } = await response.json();
